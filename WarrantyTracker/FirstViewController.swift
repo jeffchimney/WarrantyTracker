6d6ed5d05a08c9f8235254e096e6598a2a47b066
Jeff Chimney <jeff.chimney@avssystems.ca>
2016-05-20 11:10:55 -0700

    Initial Commit

---
+++ WarrantyTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WarrantyTracker
+//
+//  Created by Jeff Chimney on 2016-05-20.
+//  Copyright © 2016 Jeff Chimney. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
