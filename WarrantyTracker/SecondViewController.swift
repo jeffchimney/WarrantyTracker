a70f3fbcfb0e236dfabf9a5f277ec3faadb31261
Jeff Chimney <jeffchimney@icloud.com>
2016-05-24 20:34:42 -0700

    Implemented the 'Tesseract' image parsing framework and showed the picture you took in an imageview.  Parsed text is being printed in the terminal.

--- WarrantyTracker/SecondViewController.swift
+++ WarrantyTracker/SecondViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
     @IBOutlet var cameraView: UIImageView!
     @IBOutlet weak var captureButton: UITabBarItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        activityIndicator.hidden = true
+    }
+    
+    override func viewWillAppear(animated: Bool) {
         // Set up camera view and provide options for upload.
         let imagePickerActionSheet = UIAlertController(title: "Take or Upload a Photo",
                                                        message: nil, preferredStyle: .ActionSheet)
@@ -46,6 +50,77 @@ class SecondViewController: UIViewController, UIImagePickerControllerDelegate, U
         imagePickerActionSheet.addAction(cancelButton)
         presentViewController(imagePickerActionSheet, animated: true,
                               completion: nil)
+    }
+    
+    // preserve aspect ratio when scaling for Tesseract
+    func scaleImage(image: UIImage, maxDimension: CGFloat) -> UIImage {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        var scaleFactor: CGFloat
+        
+        if image.size.width > image.size.height {
+            scaleFactor = image.size.height / image.size.width
+            scaledSize.width = maxDimension
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            scaleFactor = image.size.width / image.size.height
+            scaledSize.height = maxDimension
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        image.drawInRect(CGRectMake(0, 0, scaledSize.width, scaledSize.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
+    
+    func addActivityIndicator() {
+        activityIndicator = UIActivityIndicatorView(frame: view.bounds)
+        activityIndicator.activityIndicatorViewStyle = .WhiteLarge
+        activityIndicator.backgroundColor = UIColor(white: 0, alpha: 0.25)
+        activityIndicator.startAnimating()
+        view.addSubview(activityIndicator)
+    }
+    
+    func removeActivityIndicator() {
+        activityIndicator.removeFromSuperview()
+        activityIndicator = nil
+    }
+    
+    func imagePickerController(picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedPhoto = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let scaledImage = scaleImage(selectedPhoto, maxDimension: 640)
+        
+        cameraView.contentMode = .ScaleAspectFit
+        cameraView.image = scaledImage
+        
+        addActivityIndicator()
+        
+        dismissViewControllerAnimated(true, completion: {
+            self.performImageRecognition(scaledImage)
+        })
+    }
+    
+    func performImageRecognition(image: UIImage) {
+        let tesseract = G8Tesseract()
+        // set language to english
+        tesseract.language = "eng"
+        // .TesseractOnly, is the fastest, least accurate method
+        //.CubeOnly is slower more accurate since it employs more artificial intelligence
+        //.TesseractCubeCombined, which runs both is slowest, most accurate
+        tesseract.engineMode = .TesseractCubeCombined
+        tesseract.pageSegmentationMode = .Auto
+        tesseract.maximumRecognitionTime = 60.0
+        // desaturates image, increases contrast
+        tesseract.image = image.g8_blackAndWhite()
+        tesseract.recognize()
+        // print the recognized text
+        print(tesseract.recognizedText)
+        //textView.editable = true
+        removeActivityIndicator()
     }
 
     override func didReceiveMemoryWarning() {
