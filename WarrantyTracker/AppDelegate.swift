0e00737c09d25f99d4295c5979102d67c1a1ff4e
Jeff Chimney <jeff.chimney@avssystems.ca>
2016-06-13 15:44:36 -0700

    Added a signing certificate for Push Notifications.  This doesnt have to live in the file structure of the project and can be removed once I'm sure I can access it from my laptop as well.  Started enabling push notifications.  Going to try upgrading xcode so i can sign to my phone

--- WarrantyTracker/AppDelegate.swift
+++ WarrantyTracker/AppDelegate.swift
@@ -17,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        // set up push notifications
+        let notificationTypes: UIUserNotificationType = [UIUserNotificationType.Alert, UIUserNotificationType.Badge, UIUserNotificationType.Sound]
+        let pushNotificationSettings = UIUserNotificationSettings(forTypes: notificationTypes, categories: nil)
+        application.registerUserNotificationSettings(pushNotificationSettings)
+        application.registerForRemoteNotifications()
+        
         // On launch, open second tab of UITabBarController
         if self.window!.rootViewController as? UITabBarController != nil {
             let tabbarController = self.window!.rootViewController as! UITabBarController
@@ -27,6 +33,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         return true
+    }
+    
+    // MARK: - Push Notification Delegate Methods
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        print("DEVICE TOKEN = \(deviceToken)")
+    }
+    
+    func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+        print(error)
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        print(userInfo)
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -50,7 +70,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
